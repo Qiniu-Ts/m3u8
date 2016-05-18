b875a1ae78b6890e3e81550dd68209108726389c
rwifeng <rw.ifeng@gmail.com>
2016-05-18 21:01:10 +0800

    refactor code

--- rdm3u8.go
+++ rdm3u8.go
@@ -12,7 +12,7 @@ import (
     "bytes"
 
     "qiniupkg.com/api.v7/kodo"
-	"qiniupkg.com/api.v7/conf"
+    "qiniupkg.com/api.v7/conf"
     "qiniupkg.com/api.v7/kodocli"
 )
 
@@ -25,18 +25,38 @@ type Config struct {
 	KeysFileLoc string `json:"keys_file_loc"`
 }
 
+type Client struct {
+	*Config
+	Kodo *kodo.Client	
+}
+
+func New(cfg *Config) (p *Client) {
+		
+	p = new(Client)
+    p.Config = cfg
+
+    //初始化AK，SK
+    conf.ACCESS_KEY = cfg.AccessKey
+    conf.SECRET_KEY = cfg.SecretKey
+
+    //创建一个Client
+    p.Kodo = kodo.New(0, nil)
+
+    return
+}
+
 func main() {
 
 	if len(os.Args) < 2 {
 		fmt.Println("./rdm3u8 <config_file_path>")
 		return
 	}
-	c, err := loadCfg(os.Args[1])
-	fmt.Println(c)
+	cfg, err := loadCfg(os.Args[1])
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(-1)
 	}
+	c := New(cfg)	
 
 	f, err := os.Open(c.KeysFileLoc)
     if err != nil {
@@ -51,22 +71,15 @@ func main() {
 	for scanner.Scan() {
 
 		key := scanner.Text()
-		u := c.url(key)
-
-		data, err := m3u8RmDomain(u)
-		if err != nil {
-			fmt.Println(err)
-	        os.Exit(2)
-		}
-
 		if c.BucketBak != "" {
 			c.Copy(key)
 		}
 
-		err = c.Up(key, data)
+
+		err := c.M3u8RmDomain(key)
 		if err != nil {
 			fmt.Println(err)
-			os.Exit(3)
+	        os.Exit(2)
 		}
 	}
 
@@ -75,7 +88,7 @@ func main() {
 	}
 }
 
-func loadCfg(path string) (cfg Config, err error) {
+func loadCfg(path string) (cfg *Config, err error) {
 
 	file, err := os.Open(path)
 	if err != nil {
@@ -87,7 +100,7 @@ func loadCfg(path string) (cfg Config, err error) {
 	return
 }
 
-func (c *Config) url(key string) string {
+func (c *Client) url(key string) string {
 	u := c.Domain + "/" + key
 	if !strings.HasPrefix(u, "http") {
 		u = "http://" + u
@@ -95,14 +108,7 @@ func (c *Config) url(key string) string {
 	return u
 }
 
-func (c *Config) Up(key string, data []byte) error {
-
-    //初始化AK，SK
-    conf.ACCESS_KEY = c.AccessKey
-    conf.SECRET_KEY = c.SecretKey
-
-    //创建一个Client
-    cli := kodo.New(0, nil)
+func (c *Client) Up(key string, data []byte) error {
 
     //设置上传的策略
     policy := &kodo.PutPolicy{
@@ -111,7 +117,7 @@ func (c *Config) Up(key string, data []byte) error {
     }
 
     //生成一个上传token
-    token := cli.MakeUptoken(policy);
+    token := c.Kodo.MakeUptoken(policy);
 
     //构建一个uploader
     zone := 0
@@ -124,36 +130,40 @@ func (c *Config) Up(key string, data []byte) error {
     return uploader.Put(nil, nil, token, key, datar, size, nil)
 }
 
-func (c *Config) Copy(key string) error {
+func (c *Client) Copy(key string) error {
 
-	conf.ACCESS_KEY = c.AccessKey
-	conf.SECRET_KEY = c.SecretKey
-
-	//new一个Bucket管理对象
-	cli := kodo.New(0, nil)
-	p := cli.Bucket(c.Bucket)
+	p := c.Kodo.Bucket(c.Bucket)
 
 	//调用Copy方法移动文件
 	return p.Conn.Call(nil, nil, "POST", p.Conn.RSHost + kodo.URICopy(c.Bucket, key, c.BucketBak, key))
 }
 
 
-func m3u8RmDomain(u string)(body []byte, err error) {
+func (c *Client) M3u8RmDomain(key string)(err error) {
 
+	u := c.url(key)
 	resp, err := http.Get(u)
 	if err != nil {
 		return
 	}
 	defer resp.Body.Close()
-	body, err = ioutil.ReadAll(resp.Body)
+
+	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return
 	}
+
 	exp, err := regexp.CompilePOSIX("^http://[^/]*")
 	if err != nil {
 		return
 	}
 
+	if !exp.Match(body) {
+		return
+	}
+
 	body = exp.ReplaceAll(body, []byte(""))
+	err = c.Up(key, body)
+
 	return
 }
